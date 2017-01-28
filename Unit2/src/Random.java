13225ef224a7828abcce063d0094d7a210c5ba3b
quietF <a.cathey.c@gmail.com>
2017-01-28 10:35:28 +0000

    Random.class up

---
+++ Unit2/src/Random.java
@@ -0,0 +1,16 @@
+/**
+ * @author Andres Cathey
+ * @category Unit2
+ *
+ */
+public class Random {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
