b0af88c40ad1a9e0ea9d997d9b1505fab4243d44
quietF <a.cathey.c@gmail.com>
2017-01-30 13:14:40 +0000

    done

--- RandomNumbers/src/Distribution.java
+++ RandomNumbers/src/Distribution.java
@@ -5,7 +5,7 @@ public class Distribution {
 		
 	}
 	
-	public double getYvalue(double x){
+	public double evaluate(double x){
 		double mu = 0.;
 		double sd = 1.;
 		return Math.exp(-(x-mu)*(x-mu) / (2 * sd * sd));
