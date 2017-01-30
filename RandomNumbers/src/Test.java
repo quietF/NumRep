55b6e5f5a729da6b285ac1bc41cb50c9683cc3e0
quietF <a.cathey.c@gmail.com>
2017-01-30 11:04:15 +0000

    get histogram

--- RandomNumbers/src/Test.java
+++ RandomNumbers/src/Test.java
@@ -1,3 +1,4 @@
+import Jama.*;
 
 public class Test {
 	
@@ -7,7 +8,21 @@ public class Test {
 		double b = 1.;
 		Random aleatorio = new Random(a, b, normal);
 		
-		aleatorio.findFmax();
+		double[] fmax = aleatorio.findFmax();
+		System.out.println(fmax[1]);
+		System.out.println(aleatorio.getRandBelow());
+		
+		int nIterations = 10000;
+		
+		//double[][] data = new double[nIterations][1];
+		
+		double[][] data = aleatorio.getData(nIterations);
+		
+		MyFileWriter fw = new MyFileWriter();
+		
+		Matrix A = new Matrix(data);
+		
+		fw.writeFile("test.txt", A);
 	}
 
 }
