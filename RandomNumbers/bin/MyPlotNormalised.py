75085740ae0097ec2b523cb4d3f81d9fec78e524
quietF <a.cathey.c@gmail.com>
2017-02-13 09:44:37 +0000

    start checkpoint 2 - ODEs

---
+++ RandomNumbers/bin/MyPlotNormalised.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np 
+import pylab as pl
+import numpy.random as rand  # Random number generation module
+
+file = sys.argv[1]
+outfile = file + ".pdf"
+
+data = np.loadtxt(file)
+
+#print data
+
+# Simple example to produce a histogram of the errors
+pl.hist(data[:], bins=50, normed=True)
+pl.savefig(outfile)
+pl.show()
+
